a55c4f2c1b705ec5a43b698295ed311a3eaeb52b
ryandeba <ryandeba@gmail.com>
2013-12-21 14:47:14 -0600

    more progress

--- QuickerClicker.js
+++ QuickerClicker.js
@@ -1,3 +1,48 @@
 $(function(){
-	QuickerClicker = new Backbone.Marionette.Application();
+	var app = new Backbone.Marionette.Application();
+
+	app.addRegions({
+		mainRegion: "#quickerclicker",
+		clickerCatcherRegion: "#clickercatcher"
+	});
+
+	var clickerCatcher = new QuickerClicker.ClickerCatcher();
+	var clickerCatcherView = new QuickerClicker.ClickerCatcherView({
+		model: clickerCatcher
+	});
+
+	var achievements = new QuickerClicker.AchievementCollection([
+		{
+			name: "test",
+			description: "this is a test",
+			statsMeetRequirements: function(){
+				return true;
+			}
+		}
+	]);
+
+	app.newGame = function(){
+		var game = new QuickerClicker.Game();
+		var gameView = new QuickerClicker.GameView({
+			model: game
+		});
+
+		this.listenTo(clickerCatcher, "click", function(){game.addClick();});
+
+		this.listenTo(game, "gameOver", this.gameEnd);
+
+		this.mainRegion.show(gameView);
+		this.clickerCatcherRegion.show(clickerCatcherView);
+	};
+
+	app.gameEnd = function(){
+		this.stopListening(clickerCatcher);
+		this.clickerCatcherRegion.reset();
+	};
+
+	app.on("initialize:after", function(){
+		this.newGame();
+	});
+
+	app.start();
 });
