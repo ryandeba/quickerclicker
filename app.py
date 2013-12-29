a92a1ab442a203feab5aeb2d51ff8d3c05d0dd0b
ryandeba <ryandeba@gmail.com>
2013-12-29 00:46:36 -0600

    remove font-awesome

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import webapp2
 import os
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class Index(webapp2.RequestHandler):
 
 	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'views/index.html')
-		self.response.write(template.render(path, {}))
+		user = users.get_current_user()
+
+		if user:
+			path = os.path.join(os.path.dirname(__file__), 'index.html')
+			self.response.write(template.render(path, {'username': user.nickname()}))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
 	('/', Index),
